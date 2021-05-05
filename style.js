84217e39fa1a2925169c9b253f0b600415f46851
Austin Hines <Austinhines0605@gmail.com>
2021-05-05 13:01:18 -0500

    Must Finish Finall touches

---
+++ style.js
@@ -0,0 +1,33 @@
+// var image  = [];
+// var breed   = [];
+// var color = [];
+// var location = [];
+
+// var imageInput  = document.getElementById("image");
+// var breedInput   = document.getElementById("breed");
+// var colorInput = document.getElementById("color");
+// var locationInput = document.getElementById("location");
+
+// var messageBox  = document.getElementById("display");
+
+// function insert ( )  {
+//  image.push( imageInput.value );
+//  breed.push( breedInput.value );
+//  color.push( colorInput.value );
+//  location.push( locationInput.value );
+  
+//  Show();
+// }
+
+// function Show () {
+//   // Clear our fields
+  
+  
+//   // Show our output
+//   messageBox.innerHTML = "";
+  
+//   messageBox.innerHTML += "Image: " + titles.join(", ") + "<br/>";
+//   messageBox.innerHTML += "Breed: " + names.join(", ") + "<br/>";
+//   messageBox.innerHTML += "Color: " + tickets.join(", ") + "<br/>";
+//   messageBox.innerHTML += "Location: " + tickets.join(", ");
+// }
